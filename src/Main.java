2f03b45c339ed3416aee7a2d9b31bd5d8ff5292b
KANGSAN_DEV <devstar1223@gmail.com>
2026-03-10 19:26:44 +0900

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+// 문제명(https://www.acmicpc.net/problem/문제번호)
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = br.readLine();
+        bw.write(N);
+
+        bw.flush();
+
+        bw.close();
+        br.close();
+    }
+}
